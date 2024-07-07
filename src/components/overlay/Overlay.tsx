25c008ccc6bb050a60c3968e2abeb866642432e6
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-07 23:32:05 +0200

    fix TS error

--- src/components/overlay/Overlay.tsx
+++ src/components/overlay/Overlay.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import { EmptyObject } from "@/types/helpers";
-
-const Overlay: React.FC<React.PropsWithChildren<EmptyObject>> = ({
-  children,
-}) => (
+const Overlay: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
   <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-black bg-opacity-50">
     {children}
   </div>
