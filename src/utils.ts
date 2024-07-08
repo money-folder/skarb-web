f1cccfb6835c058a7ab7878126df70b9d0a30c53
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-08 15:48:17 +0200

    fix default date

--- src/utils.ts
+++ src/utils.ts
@@ -21,19 +21,14 @@ export const generatePseudoUUID = () => {
   );
 };
 
-const formatMonthIndex = (monthIndex: number) => {
-  const month = monthIndex + 1;
-  if (month >= 10) {
-    return `${month}`;
-  }
-
-  return `0${month}`;
+export const getLocalISOString = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
 };
-
-export const getLocalISOString = (date: Date) =>
-  `${date.getFullYear()}-${formatMonthIndex(
-    date.getMonth()
-  )}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
 export const generateNumberArray = (min: number, max: number, step: number) => {
   const result = [];
