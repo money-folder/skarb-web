bef5e247c821f33ce6dbf4ba765c71a2c7cf88a1
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-08 16:48:11 +0200

    add whistory + buttons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { fetchCurrentUserWallets } from "@/actions/wallets";
+import CreateWhistoryButton from "@/widgets/create-whistory/CreateWhistoryButton";
 
 import NavLink from "./Navlink";
 
@@ -6,7 +7,7 @@ export default async function Sidebar() {
   const result = await fetchCurrentUserWallets();
 
   return (
-    <div className="w-[200px] h-full border-r-2 border-r-black">
+    <div className="w-[225px] h-full border-r-2 border-r-black">
       <ul className="p-5">
         <li>
           <NavLink activeClassName="text-white bg-black" href="/wallets">
@@ -17,13 +18,21 @@ export default async function Sidebar() {
         {result.data ? (
           <ul className="pl-5">
             {result.data.map((w) => (
-              <li key={w.id}>
+              <li
+                key={w.id}
+                className="w-full flex justify-between gap-2 overflow-hidden truncate"
+              >
                 <NavLink
-                  activeClassName="text-white bg-black"
+                  className="overflow-hidden text-ellipsis"
+                  activeClassName="flex-grow"
                   href={`/wallets/${w.id}`}
                 >
                   {w.name}
                 </NavLink>
+
+                <span className="w-fit shrink-0">
+                  <CreateWhistoryButton walletId={w.id} walletName={w.name} />
+                </span>
               </li>
             ))}
           </ul>
