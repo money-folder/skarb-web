ddf4dc2d8bfcdd02bc2e9734a616d8a23b42f575
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-13 18:57:13 +0200

    cleanup

--- src/widgets/whistory-changes-chart/utils.ts
+++ src/widgets/whistory-changes-chart/utils.ts
@@ -10,14 +10,15 @@ export const getWhistoryAbsChangesData = (
     const finishDate = whistoryList[i].date;
     const dateDiff = finishDate.getTime() - startDate.getTime();
     const midDate = new Date(Math.floor(startDate.getTime() + dateDiff / 2));
+    const changes = +(
+      whistoryList[i].moneyAmount - whistoryList[i - 1].moneyAmount
+    ).toFixed(2);
 
     result.unshift({
       startDate,
       midDate,
       finishDate,
-      changes: +(
-        whistoryList[i].moneyAmount - whistoryList[i - 1].moneyAmount
-      ).toFixed(2),
+      changes,
     });
   }
 
