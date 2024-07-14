ab932fc1f81d5e187cc2fb82904e5794b39a1648
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2024-07-14 19:00:32 +0200

    83-bug-signout-button-should-only-be-visible-for-the-signed-in-users (#86)

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import { fetchCurrentUserWallets } from "@/actions/wallets";
 import CreateWhistoryButton from "@/widgets/create-whistory/CreateWhistoryButton";
 import CreateWalletButton from "@/widgets/create-wallet/CreateWalletButton";
@@ -7,6 +8,7 @@ import UserProfile from "./UserProfile";
 import { SignOut } from "../SignOut";
 
 export default async function Sidebar() {
+  const session = await auth();
   const result = await fetchCurrentUserWallets();
 
   return (
@@ -14,42 +16,44 @@ export default async function Sidebar() {
       <UserProfile />
 
       <ul className="mt-5 flex-grow">
-        <li className="w-full flex justify-between gap-2 overflow-hidden truncate">
-          <NavLink activeClassName="text-white bg-black" href="/wallets">
-            Wallets
-          </NavLink>
-
-          <span className="w-fit shrink-0">
-            <CreateWalletButton />
-          </span>
-        </li>
-
         {result.data ? (
-          <ul className="pl-5">
-            {result.data.map((w) => (
-              <li
-                key={w.id}
-                className="w-full flex justify-between gap-2 overflow-hidden truncate"
-              >
-                <NavLink
-                  className="overflow-hidden text-ellipsis"
-                  activeClassName="text-white bg-black"
-                  href={`/wallets/${w.id}`}
-                >
-                  {w.name}
-                </NavLink>
+          <>
+            <li className="w-full flex justify-between gap-2 overflow-hidden truncate">
+              <NavLink activeClassName="text-white bg-black" href="/wallets">
+                Wallets
+              </NavLink>
 
-                <span className="w-fit shrink-0">
-                  <CreateWhistoryButton walletId={w.id} walletName={w.name} />
-                </span>
-              </li>
-            ))}
-          </ul>
+              <span className="w-fit shrink-0">
+                <CreateWalletButton />
+              </span>
+            </li>
+
+            <ul className="pl-5">
+              {result.data.map((w) => (
+                <li
+                  key={w.id}
+                  className="w-full flex justify-between gap-2 overflow-hidden truncate"
+                >
+                  <NavLink
+                    className="overflow-hidden text-ellipsis"
+                    activeClassName="text-white bg-black"
+                    href={`/wallets/${w.id}`}
+                  >
+                    {w.name}
+                  </NavLink>
+
+                  <span className="w-fit shrink-0">
+                    <CreateWhistoryButton walletId={w.id} walletName={w.name} />
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </>
         ) : null}
       </ul>
 
       <div className="mt-5 flex justify-center">
-        <SignOut />
+        {session?.user ? <SignOut /> : null}
       </div>
     </div>
   );
