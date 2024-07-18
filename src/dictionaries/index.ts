18936c036195c0495ec30b58838f4a9d08e89766
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-18 16:31:33 +0200

    resolve ts issues

--- src/dictionaries/index.ts
+++ src/dictionaries/index.ts
@@ -2,6 +2,8 @@ import "server-only";
 
 import { Dictionary } from "@/types/locale";
 
+// TODO: fix
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const dictionaries: any = {
   en: () => import("./en.json").then((module) => module.default),
   be: () => import("./be.json").then((module) => module.default),
--- src/types/locale.ts
+++ src/types/locale.ts
@@ -11,7 +11,58 @@ export interface Dictionary {
     };
     loadingWalletsFailed: string;
   };
-  walletHistoryPage: {
+  whistoryPage: {
     title: string;
+    filters: {
+      form: {
+        fromLabel: string;
+        toLabel: string;
+        resetLabel: string;
+      };
+    };
+    whistoryTable: {
+      balance: string;
+      date: string;
+      changes: string;
+      actions: string;
+    };
+    whistoryEmpty: string;
+    notEnoughDataForChart: string;
+  };
+  sidebar: {
+    walletsTitle: string;
+    signoutLabel: string;
+  };
+  modals: {
+    createWhistory: {
+      title: string;
+      form: {
+        dateLabel: string;
+        amountLabel: string;
+        cancelLabel: string;
+        submitLabel: string;
+      };
+    };
+    createWallet: {
+      title: string;
+      form: {
+        nameLabel: string;
+        currencyLabel: string;
+        cancelLabel: string;
+        submitLabel: string;
+      };
+    };
+  };
+  charts: {
+    whistory: {
+      tooltip: {
+        balanceLabel: string;
+      };
+    };
+    whistoryChanges: {
+      tooltip: {
+        changesLabel: string;
+      };
+    };
   };
 }
