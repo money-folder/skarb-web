b8471fb7954003d55e56c6ad76d3c9898d98e202
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-18 12:54:52 +0200

    add basic routing

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { DEFAULT_LOCALE, i18n } from "./locales";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // remove the default locale from the URL
+  if (
+    pathname.startsWith(`/${DEFAULT_LOCALE}/`) ||
+    pathname === `/${DEFAULT_LOCALE}`
+  ) {
+    return NextResponse.redirect(
+      new URL(
+        pathname.replace(
+          `/${DEFAULT_LOCALE}`,
+          pathname === `/${DEFAULT_LOCALE}` ? "/" : ""
+        ),
+        request.url
+      )
+    );
+  }
+
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  if (pathnameIsMissingLocale) {
+    return NextResponse.rewrite(
+      new URL(
+        `/${DEFAULT_LOCALE}${pathname}${request.nextUrl.search}`,
+        request.nextUrl.href
+      )
+    );
+  }
+}
+
+export const config = {
+  matcher: ["/((?!_next).*)"],
+};
