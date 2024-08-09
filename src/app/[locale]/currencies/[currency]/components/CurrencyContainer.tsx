ff7eede59d290e70540baa1bc65fc050312a077e
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2024-08-09 23:36:22 +0200

    120-currencies-show-composed-whistory-table-next-to-the-chart (#122)

--- src/app/[locale]/currencies/[currency]/components/CurrencyContainer.tsx
+++ src/app/[locale]/currencies/[currency]/components/CurrencyContainer.tsx
@@ -1,6 +1,7 @@
 import { fetchWhistoryByCurrency } from "@/actions/wallet-history";
 import { WithMounted } from "@/components/WithMounted";
 import WhistoryComposedChart from "@/widgets/whistory-composed-chart/WhistoryComposedChart";
+import CurrencyComposedTable from "./currency-composed-table/CurrencyComposedTable";
 
 interface Props {
   currency: string;
@@ -14,10 +15,20 @@ export default async function CurrencyContainer({ currency }: Props) {
   }
 
   return (
-    <div className="w-full flex justify-center">
-      <WithMounted>
-        <WhistoryComposedChart width={850} height={350} data={response.data} />
-      </WithMounted>
+    <div className="h-full w-full grid gap-5 grid-cols-[1fr,_1fr] grid-rows-[auto,_1fr]">
+      <div className="col-span-1 row-span-1 overflow-auto">
+        <CurrencyComposedTable walletHistory={response.data} />
+      </div>
+
+      <div className="col-span-1 row-span-1 flex justify-center">
+        <WithMounted>
+          <WhistoryComposedChart
+            width={650}
+            height={300}
+            data={response.data}
+          />
+        </WithMounted>
+      </div>
     </div>
   );
 }
