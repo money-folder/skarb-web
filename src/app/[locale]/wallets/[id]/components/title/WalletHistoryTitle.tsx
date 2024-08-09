c01ba6f6a2957e5fc0006a5a9cf574aa6bb20ed2
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2024-08-09 15:01:04 +0200

    118-whistory-show-wallet-name-in-the-whistory-page-title (#121)

---
+++ src/app/[locale]/wallets/[id]/components/title/WalletHistoryTitle.tsx
@@ -0,0 +1,22 @@
+import { fetchWallet } from "@/actions/wallets";
+import { replacePlaceholders } from "@/utils";
+
+interface Props {
+  pageTitleTemplate: string;
+  walletId: string;
+}
+
+export default async function WalletHistoryTitle({
+  pageTitleTemplate,
+  walletId,
+}: Props) {
+  const wallet = await fetchWallet(walletId);
+
+  return (
+    <h1 className="w-full col-span-3 row-span-1 text-center font-extrabold text-lg">
+      {replacePlaceholders(pageTitleTemplate, {
+        walletName: wallet?.data?.name ? `${wallet.data.name}` : `Wallet`,
+      })}
+    </h1>
+  );
+}
