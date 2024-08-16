444e4e2fb691f5aca8deef2abaf61494d4ab75eb
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-08-16 07:27:11 +0200

    add basic filtering UI

---
+++ src/app/[locale]/currencies/[currency]/components/CurrencyComposedFilters.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+import { getLocalISOString } from "@/utils";
+import { isValidDate } from "@/utils/time-utils";
+
+const CurrencyComposedFilters = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const { register, watch } = useForm();
+
+  const dateFromParam = searchParams.get("dateFrom")
+    ? getLocalISOString(new Date(+searchParams.get("adteFrom")!))
+    : undefined;
+
+  const dateToParam = searchParams.get("dateTo")
+    ? getLocalISOString(new Date(+searchParams.get("dateTo")!))
+    : undefined;
+
+  useEffect(() => {
+    watch((data) => {
+      const params = new URLSearchParams(searchParams);
+      if (data.dateFrom && isValidDate(data.dateFrom)) {
+        params.set("dateFrom", data.dateFrom.getTime());
+      } else {
+        params.delete("dateFrom");
+      }
+
+      if (data.dateTo && isValidDate(data.dateTo)) {
+        params.set("dateTo", data.dateTo.getTime());
+      } else {
+        params.delete("dateTo");
+      }
+
+      replace(`${pathname}?${params.toString()}`);
+    });
+  }, [watch, searchParams, replace, pathname]);
+
+  return (
+    <form>
+      <div className="flex gap-5">
+        <label className="flex gap-3">
+          <span>From</span>
+          <input
+            {...register("dateFrom", {
+              required: false,
+              valueAsDate: true,
+            })}
+            type="datetime-local"
+            name="dateFrom"
+            {...(dateFromParam ? { defaultValue: dateFromParam } : {})}
+          />
+        </label>
+        <label className="flex gap-3">
+          <span>To</span>
+          <input
+            {...register("dateTo", { required: false, valueAsDate: true })}
+            type="datetime-local"
+            name="dateTo"
+            {...(dateToParam ? { defaultValue: dateToParam } : {})}
+          />
+        </label>
+      </div>
+    </form>
+  );
+};
+
+export default CurrencyComposedFilters;
--- src/app/[locale]/currencies/[currency]/page.tsx
+++ src/app/[locale]/currencies/[currency]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import CurrencyContainer from "./components/CurrencyContainer";
+import CurrencyComposedFilters from "./components/CurrencyComposedFilters";
 
 interface Props {
   params: { currency: string; locale: string };
@@ -11,6 +12,10 @@ export default async function CurrencyPage({ params: { currency } }: Props) {
       <h1 className="w-full col-span-3 row-span-1 text-center font-extrabold text-lg">
         {currency}
       </h1>
+
+      <div className="p-2 w-full flex justify-start items-center col-span-2 row-span-1 bg-gray-200 rounded-lg">
+        <CurrencyComposedFilters />
+      </div>
 
       <div className="mt-10 h-full row-span-1 col-span-2 flex gap-5 overflow-hidden">
         <Suspense fallback={null}>
