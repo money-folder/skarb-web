603373465dd521671645e36cfbb00efdc5eff0fd
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-09-29 18:28:29 +0200

    simplify the lint-n-build

--- src/app/[locale]/currencies/[currency]/components/CurrencyContainer.tsx
+++ src/app/[locale]/currencies/[currency]/components/CurrencyContainer.tsx
@@ -1,8 +1,8 @@
-import { fetchWhistoryByCurrency } from "@/actions/wallet-history";
-import { WithMounted } from "@/components/WithMounted";
-import WhistoryComposedChart from "@/widgets/whistory-composed-chart/WhistoryComposedChart";
-import CurrencyComposedTable from "./currency-composed-table/CurrencyComposedTable";
-import WhistoryComposedChangesChart from "@/widgets/whistory-composed-changes-chart/WhistoryComposedChangesChart";
+import { fetchWhistoryByCurrency } from '@/actions/wallet-history';
+import { WithMounted } from '@/components/WithMounted';
+import WhistoryComposedChart from '@/widgets/whistory-composed-chart/WhistoryComposedChart';
+import CurrencyComposedTable from './currency-composed-table/CurrencyComposedTable';
+import WhistoryComposedChangesChart from '@/widgets/whistory-composed-changes-chart/WhistoryComposedChangesChart';
 
 interface Props {
   currency: string;
@@ -10,11 +10,7 @@ interface Props {
   toTs?: number;
 }
 
-export default async function CurrencyContainer({
-  currency,
-  fromTs,
-  toTs,
-}: Props) {
+export default async function CurrencyContainer({ currency, fromTs, toTs }: Props) {
   const response = await fetchWhistoryByCurrency(currency, { fromTs, toTs });
 
   if (!response.data) {
@@ -22,26 +18,18 @@ export default async function CurrencyContainer({
   }
 
   return (
-    <div className="h-full w-full grid gap-5 grid-cols-[1fr,_1fr] grid-rows-[auto,_1fr]">
+    <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[auto,_1fr] gap-5">
       <div className="col-span-1 row-span-1 overflow-auto">
         <CurrencyComposedTable walletHistory={response.data} />
       </div>
 
-      <div className="overflow-y-auto col-span-1 row-span-1 flex flex-col justify-start items-center">
+      <div className="col-span-1 row-span-1 flex flex-col items-center justify-start overflow-y-auto">
         <WithMounted>
-          <WhistoryComposedChart
-            width={650}
-            height={300}
-            data={response.data}
-          />
+          <WhistoryComposedChart width={650} height={300} data={response.data} />
         </WithMounted>
 
         <WithMounted>
-          <WhistoryComposedChangesChart
-            width={650}
-            height={300}
-            data={response.data}
-          />
+          <WhistoryComposedChangesChart width={650} height={300} data={response.data} />
         </WithMounted>
       </div>
     </div>
