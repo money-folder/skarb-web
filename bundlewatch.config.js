5a20cc5567e6517f7bdeff88d2956beb54308f25
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2024-11-21 23:15:55 +0100

    181-chore-setup-nextbundle-analyzer (#184)
    * add bundlewatch
    
    * okay, don't create a comment (for now)
    
    * update the base branch
    
    * remove env
    
    * more config changes
    
    * add some env
    
    * add writing a comment
    
    * update on
    
    * test message
    
    * get correct repo name
    
    * add permissions
    
    * test
    
    * comment out extra stuff
    
    * add more permissions
    
    * add writing bundlewatch
    
    * formatting
    
    * formatting

---
+++ bundlewatch.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  files: [
+    {
+      path: ".next/static/chunks/*.js",
+      maxSize: "100kb",
+    },
+    {
+      path: ".next/static/css/*.css",
+      maxSize: "20kb",
+    },
+    {
+      path: ".next/server/pages/*.js",
+      maxSize: "100kb",
+    },
+  ],
+  ci: {
+    trackBranches: ["main", "develop"],
+  },
+};
