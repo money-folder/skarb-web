cba2bd755adb030cc29844f70302bcf92663278a
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-11-23 17:29:10 +0100

    okay, this is a build issue for sure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as Material from "@mui/material";
-
 export default function Home() {
   return null;
 }
--- src/app/[locale]/wallets/page.tsx
+++ src/app/[locale]/wallets/page.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as Material from "@mui/material";
+
 import { Suspense } from "react";
 
 import { getDictionary } from "@/dictionaries";
@@ -18,6 +21,8 @@ export default async function Wallets({ params: { locale } }: Props) {
       <h1 className="w-full text-center font-extrabold text-lg">
         {d.walletsPage.title}
       </h1>
+
+      <Material.Button />
 
       <div className="mt-10 w-full flex flex-col items-center">
         <div className="w-10/12">
