168f64de51c4a72148197c38c895db10c6e711a2
Aliaksandr Kavalenka <40535982+slnchn@users.noreply.github.com>
2024-11-23 21:07:33 +0100

    fix-build-size-issue (#190)

--- src/app/[locale]/wallets/page.tsx
+++ src/app/[locale]/wallets/page.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as Material from "@mui/material";
-
 import { Suspense } from "react";
 
 import { getDictionary } from "@/dictionaries";
@@ -21,8 +18,6 @@ export default async function Wallets({ params: { locale } }: Props) {
       <h1 className="w-full text-center font-extrabold text-lg">
         {d.walletsPage.title}
       </h1>
-
-      <Material.Button />
 
       <div className="mt-10 w-full flex flex-col items-center">
         <div className="w-10/12">
