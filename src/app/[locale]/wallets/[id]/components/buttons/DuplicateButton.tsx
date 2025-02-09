ee7b30821a3613d936df017517f1fbf38c56a731
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-02-09 22:05:22 +0100

    throttle duplicate button

--- src/app/[locale]/wallets/[id]/components/buttons/DuplicateButton.tsx
+++ src/app/[locale]/wallets/[id]/components/buttons/DuplicateButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 import { duplicate } from "@/app/[locale]/wallets/[id]/actions";
 import DuplicateIcon from "@/assets/duplicate.svg";
@@ -11,10 +12,29 @@ interface Props {
 }
 
 const DuplicateButton = ({ whistoryId, walletId }: Props) => {
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    let timer = undefined;
+    if (disabled) {
+      timer = setTimeout(() => {
+        setDisabled(false);
+      }, 1500);
+    }
+
+    return () => clearTimeout(timer);
+  }, [disabled]);
+
+  const onClick = () => {
+    setDisabled(true);
+    duplicate(whistoryId, walletId);
+  };
+
   return (
     <button
-      className="h-4 w-4 cursor-pointer opacity-70 hover:opacity-100"
-      onClick={() => duplicate(whistoryId, walletId)}
+      className="h-4 w-4 cursor-pointer opacity-70 hover:opacity-100 disabled:cursor-default disabled:opacity-30"
+      onClick={onClick}
+      disabled={disabled}
     >
       <Image src={DuplicateIcon} width={16} height={16} alt="duplicate" />
     </button>
