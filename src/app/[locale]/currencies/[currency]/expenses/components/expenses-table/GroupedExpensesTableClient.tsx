60c4faa2efcd36b10271c7719dc32ed1cfa5550e
Aliaksandr Kavalenka <40535982+aneekee@users.noreply.github.com>
2025-08-30 21:21:15 +0200

    318-expenses-keep-expense-tables-open-by-default (#324)

--- src/app/[locale]/currencies/[currency]/expenses/components/expenses-table/GroupedExpensesTableClient.tsx
+++ src/app/[locale]/currencies/[currency]/expenses/components/expenses-table/GroupedExpensesTableClient.tsx
@@ -61,6 +61,7 @@ export const GroupedExpensesTableClient = ({
           totalAmount={totalAmount}
           currency={currency}
           dictionary={dictionary}
+          initialExpanded
         />
       ))}
     </div>
