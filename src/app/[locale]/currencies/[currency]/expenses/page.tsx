fa6854827eb72f2435a3e89aa772416af69d82d5
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-12-23 12:14:04 +0100

    remove old date filters

--- src/app/[locale]/currencies/[currency]/expenses/page.tsx
+++ src/app/[locale]/currencies/[currency]/expenses/page.tsx
@@ -1,5 +1,5 @@
 import { Locale } from "@/locale";
-import ExpensesComposedFilters from "./components/ExpensesComposedFilters";
+
 import ExpensesContainer from "./components/ExpensesContainer";
 
 interface Props {
@@ -17,11 +17,7 @@ export default async function ExpensesPage(props: Props) {
   const { locale, currency } = params;
 
   return (
-    <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[auto,_auto,_1fr] gap-x-5 overflow-hidden">
-      <div className="col-span-2 row-span-1 flex w-full items-center justify-start rounded-lg bg-gray-200 p-2">
-        <ExpensesComposedFilters />
-      </div>
-
+    <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[auto,_1fr] gap-x-5 overflow-hidden">
       <div className="col-span-2 row-span-1 flex h-full overflow-hidden pt-5">
         <ExpensesContainer
           locale={locale}
