d4e25dd83f78d45d834e0717422277e323a0c931
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-12-25 12:59:53 +0100

    cleanup

--- src/app/[locale]/currencies/[currency]/history/components/CurrencyContainer.tsx
+++ src/app/[locale]/currencies/[currency]/history/components/CurrencyContainer.tsx
@@ -1,9 +1,5 @@
-import WhistoryComposedChangesChart from "@/app/[locale]/currencies/[currency]/history/components/whistory-composed-changes-chart/WhistoryComposedChangesChart";
 import WhistoryComposedChart from "@/app/[locale]/currencies/[currency]/history/components/whistory-composed-chart/WhistoryComposedChart";
-import { getDictionary } from "@/dictionaries";
 import { Locale } from "@/locale";
-import WalletChangesSummaryCard from "@/shared/components/cards/WalletChangesSummaryCard";
-import WhistoryEntriesSummaryCard from "@/shared/components/cards/WhistoryEntriesSummaryCard";
 import { WithMounted } from "@/shared/components/WithMounted";
 import {
   CHART_HEIGHT_DEFAULT,
@@ -26,37 +22,13 @@ export default async function CurrencyContainer({
   fromTs,
   toTs,
 }: Props) {
-  const d = await getDictionary(locale, "currencyPage");
-
   const response = await fetchCurrencyWhistory(currency, { fromTs, toTs });
   if (!response.data || !response.data.composedWhistory.length) {
     return null;
   }
 
   return (
-    <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[auto,_1fr] gap-5">
-      <div className="col-span-2 row-span-1 flex gap-5">
-        <WalletChangesSummaryCard
-          text={d.cards.changesSummary.title}
-          increases={response.data.increasesSum}
-          decreases={response.data.decreasesSum}
-          diff={response.data.increasesDecreasesDiff}
-        />
-
-        {response.data.composedWhistory.length ? (
-          <WhistoryEntriesSummaryCard
-            locale={locale}
-            startDate={response.data.composedWhistory[0].date}
-            endDate={
-              response.data.composedWhistory[
-                response.data.composedWhistory.length - 1
-              ].date
-            }
-            entriesCount={response.data.composedWhistory.length}
-          />
-        ) : null}
-      </div>
-
+    <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[1fr] gap-5">
       <div className="col-span-1 row-span-1 overflow-auto">
         <CurrencyComposedTable
           locale={locale}
@@ -67,14 +39,6 @@ export default async function CurrencyContainer({
       <div className="col-span-1 row-span-1 flex flex-col items-center justify-start overflow-y-auto">
         <WithMounted>
           <WhistoryComposedChart
-            width={CHART_WIDTH_DEFAULT}
-            height={CHART_HEIGHT_DEFAULT}
-            data={response.data.composedWhistory}
-          />
-        </WithMounted>
-
-        <WithMounted>
-          <WhistoryComposedChangesChart
             width={CHART_WIDTH_DEFAULT}
             height={CHART_HEIGHT_DEFAULT}
             data={response.data.composedWhistory}
--- src/app/[locale]/currencies/[currency]/history/page.tsx
+++ src/app/[locale]/currencies/[currency]/history/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 
 import { Locale } from "@/locale";
 
-import CurrencyComposedFilters from "./components/CurrencyComposedFilters";
 import CurrencyContainer from "./components/CurrencyContainer";
 
 interface Props {
@@ -18,10 +17,6 @@ export default async function HistoryPage(props: Props) {
 
   return (
     <div className="grid h-full w-full grid-cols-[1fr,_1fr] grid-rows-[auto,_auto,_1fr] gap-x-5 overflow-hidden">
-      <div className="col-span-2 row-span-1 flex w-full items-center justify-start rounded-lg bg-gray-200 p-2">
-        <CurrencyComposedFilters />
-      </div>
-
       <div className="col-span-2 row-span-1 flex h-full gap-5 overflow-hidden pt-5">
         <Suspense fallback={null}>
           <CurrencyContainer
